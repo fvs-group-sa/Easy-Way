2dd4792c60dfa674a6d06a49211e6cd5dd0c4c28
FVS-Group <fvs.group.sa@gmail.com>
2021-03-09 18:19:56 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::group(['/en'], function($lang) {
     })->name('home');
 
 
-    Route::get('/services', function () {
+    Route::get('en/services', function () {
         return view('pages/services');
     })->name('services');
 
@@ -60,50 +60,4 @@ Route::group(['/en'], function($lang) {
 
 
     Route::get('en/blog', [BlogController::class, 'index'])->name('blog');
-});
-
-
-
-// Arabic Routes 
-Route::group(['/'], function($lang) {
-    App::setLocale('ar');
-
-Route::get('/', function () {
-    return view('pages/index');
-})->name('home');
-
-
-Route::get('/services', function () {
-    return view('pages/services');
-})->name('services');
-
-Route::get('/settings', function () {
-    return view('admin/settings');
-})->name('settings')->middleware('auth');
-
-Route::get('/post', [PostsController::class, 'index'])->name('post')->middleware('auth');
-Route::get('/newpost', [PostsController::class, 'newPostIndex'])->name('post.new')->middleware('auth');
-Route::post('/Addpost', [PostsController::class, 'store'])->name('addpost')->middleware('auth');
-Route::get('/editpost/{post}', [PostsController::class, 'editPost'])->name('editpost')->middleware('auth');
-Route::post('/updatepost/{post}', [PostsController::class, 'update'])->name('updatepost')->middleware('auth');
-Route::delete('/post/{post}', [PostsController::class, 'destroy'])->name('post.destroy')->middleware('auth');
-
-Route::get('/admin', [PostsController::class, 'index'])
-    ->name('admin')
-    ->middleware('auth');
-
-Route::get('/settings', [SettingsController::class, 'index'])
-    ->name('settings')
-    ->middleware('auth');
-
-Route::post('/logout', [LogoutController::class, 'store'])->name('logout')->middleware('auth');
-
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
-
-// Route::get('/register', [RegisterController::class, 'index'])->name('register');
-// Route::post('/register', [RegisterController::class, 'store']);
-
-
-    Route::get('ar/blog', [BlogController::class, 'index'])->name('blog');
 });
