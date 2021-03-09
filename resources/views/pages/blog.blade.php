c9595756217e32e878ade921ef979888bb22c93d
FVS-Group <fvs.group.sa@gmail.com>
2021-03-09 17:11:22 +0300

    route fixing

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -1,17 +1,4 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="css/app.css">
-
-        <!-- =====BOX ICONS===== -->
-        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
-
-        <title>EasyWay</title>
-    </head>
-    <body> --}}
         
 @extends('layouts.app')
 
@@ -54,75 +41,4 @@
     </div>
 </div>
 @endsection
-        {{-- <!--===== HEADER =====-->
-        <header class="l-header">
-            <nav class="nav bd-grid">
-            
-                <div class="logo">
-                    <!-- <h1>EasyWay<span>Transport</span></h1> -->
-                    <a href="index.html">
-                    <img src="{{ url('image/easy2-removebg-preview.png') }}" alt="">
-                    
-                   </div>
         
-                  <div class="nav__menu" id="nav-menu">
-                    <ul class="nav__list">
-                        <li class="nav__item"><a href="index.html" class="nav__link active">Home</a></li>
-                        
-                         </ul>
-                         </div>
-               
-                <div class="nav__toggle" id="nav-toggle">
-                    <i class='bx bx-menu'></i>
-                </div>
-            </nav>
-            
-        </header>
-  
-
-        
-
-
-        <section class="header">
-            <h2>الأخبار والأحداث</h2>
-          
-          
-          <div class="row">
-            <div class="leftcolumn">
-              <div class="card">
-                <h2>الدرب السهل للنقليات</h2>
-                <h5>Title description, Dec 7, 2017</h5>
-                <img src="{{ url('image/rsz_2شاحنة-removebg-preview.png') }}" alt="">
-                
-                <p>Some text..</p>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-              </div>
-              
-            </div>
-            
-
-        
-
-        </section>
-
-          <!--===== FOOTER =====-->
-          <footer class="footer">
-            <!-- <p class="footer__title">Marlon</p> -->
-            <div class="footer__social">
-                <a href="#" class="footer__icon"><i class='bx bxl-facebook' ></i></a>
-                <a href="#" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
-                <a href="#" class="footer__icon"><i class='bx bxl-twitter' ></i></a>
-            </div>
-            <p>&#169; Easyway 2021 copyright all right reserved</p>
-        </footer>
-
-
-        <!--===== SCROLL REVEAL =====-->
-        <script src="https://unpkg.com/scrollreveal"></script>
-
-        <!--===== MAIN JS =====-->
-        <script src="js/app.js"></script> --}}
-
-        
-    {{-- </body> --}}
-{{-- </html>  --}}
--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,59 @@ use App\Http\Controllers\Admin\SettingsController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function ($lang) {
-    App::setLocale($lang);
-    return view('pages/index');
-})->name('home');
+Route::group(['/en'], function($lang) {
+    App::setLocale('en');
 
+    Route::get('/en', function () {
+        return view('pages/index');
+    })->name('home');
+
+
+    Route::get('/services', function () {
+        return view('pages/services');
+    })->name('services');
+
+    Route::get('/settings', function () {
+        return view('admin/settings');
+    })->name('settings')->middleware('auth');
+
+    Route::get('/post', [PostsController::class, 'index'])->name('post')->middleware('auth');
+    Route::get('/newpost', [PostsController::class, 'newPostIndex'])->name('post.new')->middleware('auth');
+    Route::post('/Addpost', [PostsController::class, 'store'])->name('addpost')->middleware('auth');
+    Route::get('/editpost/{post}', [PostsController::class, 'editPost'])->name('editpost')->middleware('auth');
+    Route::post('/updatepost/{post}', [PostsController::class, 'update'])->name('updatepost')->middleware('auth');
+    Route::delete('/post/{post}', [PostsController::class, 'destroy'])->name('post.destroy')->middleware('auth');
+
+    Route::get('/admin', [PostsController::class, 'index'])
+        ->name('admin')
+        ->middleware('auth');
+
+    Route::get('/settings', [SettingsController::class, 'index'])
+        ->name('settings')
+        ->middleware('auth');
+
+    Route::post('/logout', [LogoutController::class, 'store'])->name('logout')->middleware('auth');
+
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+
+    // Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    // Route::post('/register', [RegisterController::class, 'store']);
+
+
+    Route::get('en/blog', [BlogController::class, 'index'])->name('blog');
+});
+
+
+
+// Arabic Routes 
+Route::group(['/'], function($lang) {
+    App::setLocale('ar');
 
 Route::get('/', function () {
-    App::setlocale('ar');
     return view('pages/index');
 })->name('home');
 
-Route::get('/', function () {
-    return view('pages/index');
-})->name('home');
 
 Route::get('/services', function () {
     return view('pages/services');
@@ -66,14 +105,5 @@ Route::post('/login', [LoginController::class, 'store']);
 // Route::post('/register', [RegisterController::class, 'store']);
 
 
-// Route::get('/blog', [BlogController::class, 'index'])->name('blog');
-Route::group(['/en'], function($lang) {
-    App::setLocale('en');
-
-    Route::get('en/blog', [BlogController::class, 'index'])->name('blog');
+    Route::get('ar/blog', [BlogController::class, 'index'])->name('blog');
 });
-
-// Route::group(['prefix'=>'{lang}'], (function ($lang) {
-//     App::setLocale($lang)
-
-// });
