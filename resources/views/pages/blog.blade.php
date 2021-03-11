86eaeddd418e5525ddc0e046b5054456324482a4
FVS-Group <fvs.group.sa@gmail.com>
2021-03-11 11:50:02 +0300

    fixed blog issue

---
+++ resources/views/pages/blog.blade.php
@@ -0,0 +1,44 @@
+
+        
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="container mx-auto px-6 py-8">
+            <div class="flex flex-wrap px-6">
+
+              {{-- Begining of Post 1 --}}
+              @if ($blogs->count())
+                @foreach($blogs as $blog)
+                  <div class="w-full lg:w-1/2   md:px-4 lg:px-6 py-5">
+                    <div class="bg-white hover:shadow-xl">  
+                      <div class="">
+                          <img src="{{asset('image/'.$blog->image)}}" alt="" class="h-96 w-full border-white border-8">
+                      </div>
+                      
+                      <div class="px-4 py-4 md:px-10">
+                        <h1 class="font-bold text-lg">
+                            {{$blog->title}}
+                        </h1>
+                        <p class="py-4">
+                          {{$blog->body}}
+                        </p>
+                        
+                        <div class="flex flex-wrap pt-8">
+                            <div class="w-full md:w-1/3 text-sm font-medium">
+                              {{$blog->created_at->diffForHumans()}}
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                @endforeach
+                {{-- End of Post 1 --}}
+              @else
+                  There is no post
+                  
+                @endif
+    </div>
+</div>
+@endsection
+        
